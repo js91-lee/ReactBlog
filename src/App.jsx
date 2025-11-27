339156c38e927b731beb36e02a772b71f44a79c4
이준서 <tmdqksdl2000@naver.com>
2025-11-27 09:02:06 +0900

    세번째 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   
-  let post = '강남 우동 맛집';
-  let [글제목, b] = useState('남자 코트추천');
-  let [글제목1, b1] = useState('강남 우동맛집');
-  let [글제목2, b2] = useState('파이썬독학');
+  let [글제목, 글제목변경] = useState(['남자 코트추천', '강남 우동맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState([0,0,0]);
+  let 
+  let [modal, setModal] = useState(false);
 
   return (
    <div className='App'>
     <div className='black-nav'>
       <h4>ReactBlog</h4>
     </div>
-    <div className='list'>
-      <h4>{ 글제목 }</h4>
+
+    <button onClick={()=>{
+      let copy = [...글제목];
+      copy[0] = '여자 코트추천';
+      글제목변경(copy);
+    }}>글수정</button>
+
+    <button onClick={ ()=>{ 
+      let copy = [...글제목];
+      copy.sort();
+      글제목변경(copy)
+    } }> 정렬버튼 </button>
+  
+    
+    {/* <div className='list'>
+      <h4>{ 글제목[0] }<span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span>
+      {따봉} </h4>
       <p>2월 17일 발행</p>
     </div>
     <div className='list'>
-      <h4>{ 글제목1 }</h4>
+      <h4>{ 글제목[1] }</h4>
       <p>2월 17일 발행</p>
     </div>
     <div className='list'>
-      <h4>{ 글제목2 }</h4>
+      <h4 onClick={()=>{ setModal(!modal) }}>{ 글제목[2] }</h4>
       <p>2월 17일 발행</p>
-    </div>
+    </div> */}
+
+    {
+      글제목.map(function(a, i){
+        return (
+          <div className='list' key={i}>
+            <h4 onClick={()=>{ setModal(!modal) }}>{ 글제목[i] }</h4><h4><span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span>{따봉[i]}</h4>
+            <p>2월 17일 발행</p>
+          </div>
+        )
+      })
+    }
+    
+    { 
+      modal == true ? <Modal></Modal> : null
+    }
    </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  ) 
 }
 
 export default App
